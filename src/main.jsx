be2b7db734951431d493a66a4703a5616fb8a948
Mofeleng <mofelengmokgope@gmail.com>
2024-01-09 20:39:25 +0200

    Created and initialised the app Removed app.css(Only need index.css) Removed App.jsx and setup the react router Created a logo and favicon and used favicon in the index.html file. Let's get working

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path='/' element={
+          <Home />
+        } />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+)

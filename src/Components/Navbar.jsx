5f17a339fa48fe04b72eb712533f7468905cd5da
Mofeleng <mofelengmokgope@gmail.com>
2024-01-09 21:15:19 +0200

    Made and styled the navbar for desktop screens

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../css/Navbar.css'
+import { Link } from 'react-router-dom';
 
-function Navbar() {
+const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+
+  const handleMobileMenuToggle = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen)
+  };
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar">
+      <div className="logo">
+        <img src="/src/assets/logo.svg" alt="Logo" />
+      </div>
+      <ul className={`nav-links ${isMobileMenuOpen ? 'mobile-menu-open' : ''}`}>
+        <li><Link to="#">My projects</Link></li>
+        <li><Link to="#" className='btn btn_primary'>Hire Me.</Link></li>
+      </ul>
+      <div className="hamburger-menu" onClick={handleMobileMenuToggle}>
+        <div className={`bar ${isMobileMenuOpen ? 'open' : ''}`}></div>
+        <div className={`bar ${isMobileMenuOpen ? 'open' : ''}`}></div>
+        <div className={`bar ${isMobileMenuOpen ? 'open' : ''}`}></div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
+import Layout from './Layout'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path='/' element={
-          <Home />
+          <Layout>
+            <Home />
+          </Layout>
         } />
       </Routes>
     </Router>

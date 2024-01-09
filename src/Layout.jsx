63de8a950fdcb769d24b8919503c7b596b83b2be
Mofeleng <mofelengmokgope@gmail.com>
2024-01-09 20:47:29 +0200

    Created the navbar and footer components Modified the layout to accept the other pages as children so the navbar and footer are always displayed

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
 
 function Layout( { children }) {
   return (
-    <div>Layout</div>
+    <>
+        <Navbar />
+            { children }
+        <Footer />
+    </>
   )
 }
 

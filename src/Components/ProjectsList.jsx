1dbc9649600608e4d84927fa0bf6b7546ff2ea62
Mofeleng <mofelengmokgope@gmail.com>
2024-01-10 22:56:30 +0200

    Finished projects list component Moved the projects list to its own component Setup graphcms code to fetch the projects from the cms

---
+++ src/Components/ProjectsList.jsx
@@ -0,0 +1,111 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { GraphQLClient, gql } from 'graphql-request'
+
+function ProjectsList() {
+  const ENDPOINT = import.meta.env.VITE_GRAPHCMS_ENDPOINT;
+  const [ projects, setProjects ] = useState([]);
+
+  const [ loading, setLoading ] = useState(true);
+  const [ error, setError ] = useState(null);
+
+  const fetchProjects = async () => {
+    try {
+      const graphqlClient = new GraphQLClient(ENDPOINT);
+
+      /*const fetchProjects = gql`
+        query GetProjects {
+          projects {
+            id
+            name
+            preview
+            slug
+            type
+            date
+            client
+            content {
+              html
+            }
+            coverPhoto {
+              url
+            }
+            languages {
+              language
+            }
+          }
+        }
+      `;*/
+
+      const fetchProjects = gql`
+        query GetProjects {
+          projects {
+            id
+            name
+            slug
+            type
+            coverPhoto {
+              url
+            }
+            languages {
+              language
+              styleName
+            }
+            preview
+          }
+        }
+      `;
+
+      const response = await graphqlClient.request(fetchProjects);
+      const res = await response;
+
+      if (res.projects) {
+        setProjects(res.projects);
+        setLoading(false);
+        setError(null);
+      }
+    } catch (error) {
+      console.log("Something went wrong", error)
+      setError("Something went wrong while fetching projects");
+      setLoading(false);
+    }
+  }
+
+  let ran = 0;
+
+  useEffect(() => {
+    if ( ran === 0) {
+      fetchProjects();
+      ran++;
+    }
+  }, []);
+
+  if (loading) {
+    return "Loading";
+  }
+
+  if (error) {
+    return "Something went wrong " + error;
+  }
+  return (
+    <div className="projects_main">
+      { projects.map((project) => (
+        <div className="project_main">
+          <h2 className="heading_small">
+            <Link to={`/project/${project.slug}`}>{ project.name } </Link>
+            <span className="type">{ project.type }</span>
+          </h2>
+          <p>
+            { project.preview }
+          </p>
+          <div className="more_info">
+            { project.languages.map((lang) => (
+              <span className={`type ${lang.styleName}`}>{ lang.language }</span>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default ProjectsList
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import '../css/Projects.css'
 import { Link } from 'react-router-dom'
+import ProjectsList from '../Components/ProjectsList'
+
 
 function Projects() {
   return (
@@ -9,37 +11,7 @@ function Projects() {
         <div className="container">
           <h1 className="heading">View all the projects I have worked on so far</h1>
           <p className="paragraph centered">Below are case studies of all my projects. Hope you like it.</p>
-          <div className="projects_main">
-            <div className="project_main">
-              <h2 className="heading_small">
-                <Link to={`/project/`}>Parawild Edu Capture</Link>
-                <span className="type">Web development</span>
-              </h2>
-              <p>
-              A responsive website for a wildlife managementcompany based in Hoedspruit
-              </p>
-              <div className="more_info">
-                <span className="type vite">Vite</span>
-                <span className="type react">React Js</span>
-                <span className="type graphql">GraphQL</span>
-              </div>
-            </div>
-
-            <div className="project_main">
-            <h2 className="heading_small">
-                <Link to={`/project/`}>Bold blossoms</Link>
-                <span className="type">Web development</span>
-              </h2>
-              <p>
-              A responsive website for a Pageantry website that hosts yearly pageants
-              </p>
-              <div className="more_info">
-                <span className="type vite">Vite</span>
-                <span className="type react">React Js</span>
-                <span className="type graphql">GraphQL</span>
-              </div>
-            </div>
-          </div>
+          <ProjectsList />
         </div>
       </section>
     </>

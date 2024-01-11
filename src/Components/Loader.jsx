46fca3f9cfb4dd56fb9980ebfecfc0acf8de9ddc
Mofeleng <mofelengmokgope@gmail.com>
2024-01-11 20:01:13 +0200

    Created Loader components and styled it

---
+++ src/Components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/Loader.css'; // Import your CSS file
+
+const Loader = () => {
+  return (
+    <div className="page-loader-container">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Loader;
